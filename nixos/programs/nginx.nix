49576420ff7ee6cb8f90dd6f638a88b340ba9d2b
Ivan Reshetnikov <ordinarydev@protonmail.com>
2023-08-18 18:26:54 +0500

    Add hsts config

--- nixos/programs/nginx.nix
+++ nixos/programs/nginx.nix
@@ -7,6 +7,15 @@
     recommendedGzipSettings = true;
     recommendedProxySettings = true;
 
+    commonHttpConfig = ''
+      # Add HSTS header with preloading to HTTPS requests.
+      # Adding this header to HTTP requests is discouraged
+      map $scheme $hsts_header {
+          https   "max-age=31536000; includeSubdomains; preload";
+      }
+      add_header Strict-Transport-Security $hsts_header;
+    '';
+
     virtualHosts = {
       "nc.comfycamp.space" = {
         useACMEHost = "comfycamp.space";
