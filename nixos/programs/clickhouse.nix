5ab727ddf00a79216930a23d495b2207290d887c
Ivan Reshetnikov <ordinarydev@protonmail.com>
2023-08-19 22:01:44 +0500

    Change clickhouse settings

--- nixos/programs/clickhouse.nix
+++ nixos/programs/clickhouse.nix
@@ -3,4 +3,18 @@
   services.clickhouse = {
     enable = true;
   };
+
+  # See https://github.com/NixOS/nixpkgs/pull/186667
+  # and https://github.com/plausible/hosting/blob/master/clickhouse/clickhouse-config.xml
+  environment.etc = {
+    "clickhouse-server/config.d/logging.xml" = {
+      text = ''
+        <clickhouse>
+          <logger>
+            <level>warning</level>
+          </logger>
+        </clickhouse>
+      '';
+    };
+  };
 }
