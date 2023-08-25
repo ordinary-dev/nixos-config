6205b3da847d5fda3e73b73caef6c0523c80c4ab
Ivan Reshetnikov <ordinarydev@protonmail.com>
2023-08-25 22:40:13 +0500

    Bump microboard to v0.0.5

--- nixos/programs/microboard.nix
+++ nixos/programs/microboard.nix
@@ -5,7 +5,7 @@ in
 {
   virtualisation.oci-containers.containers.microboard = {
     autoStart = true;
-    image = "ghcr.io/ordinary-dev/microboard:v0.0.4";
+    image = "ghcr.io/ordinary-dev/microboard:v0.0.5";
     ports = ["55006:8000"];
     user = "986:983";
     volumes = [
