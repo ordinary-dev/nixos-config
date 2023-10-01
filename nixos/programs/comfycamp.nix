fea1e7d5ab382cf93eead259d9b3fa284ce469ee
Ivan Reshetnikov <ordinarydev@protonmail.com>
2023-10-01 13:18:07 +0500

    Bump comfycamp container to v0.0.4

--- nixos/programs/comfycamp.nix
+++ nixos/programs/comfycamp.nix
@@ -2,7 +2,7 @@
 {
   virtualisation.oci-containers.containers.comfycamp = {
     autoStart = true;
-    image = "ghcr.io/ordinary-dev/comfycamp:v0.0.2";
+    image = "ghcr.io/ordinary-dev/comfycamp:v0.0.4";
     ports = ["55007:80"];
   };
 }
