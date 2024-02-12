3876aea96c9e38621e5f91fcae85aae0bcb528f6
Ivan Reshetnikov <ordinarydev@protonmail.com>
2024-02-12 23:53:53 +0500

    feat: bump my website to v0.5.0

--- nixos/programs/comfycamp.nix
+++ nixos/programs/comfycamp.nix
@@ -2,7 +2,7 @@
 {
   virtualisation.oci-containers.containers.comfycamp = {
     autoStart = true;
-    image = "ghcr.io/ordinary-dev/comfycamp:v0.4.0";
+    image = "ghcr.io/ordinary-dev/comfycamp:v0.5.0";
     ports = ["55007:80"];
   };
 }
