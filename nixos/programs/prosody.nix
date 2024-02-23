e3d0349e7af1e539092f31bc541d33201775368b
Ivan Reshetnikov <ordinarydev@protonmail.com>
2024-02-23 15:30:44 +0500

    chore(prosody): specify correct name for storage_sql module

--- nixos/programs/prosody.nix
+++ nixos/programs/prosody.nix
@@ -42,7 +42,7 @@
       "lumin@xmpp.comfycamp.space"
     ];
 
-    extraModules = [ "mod_storage_sql" ];
+    extraModules = [ "storage_sql" ];
     extraConfig = ''
       storage = "sql"
       sql = { driver = "PostgreSQL", database = "prosody", username = "prosody", password = "", host = "/run/postgresql" }
