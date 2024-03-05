9c76c57d9ebec24a37ea56f63c5cbd01525e48d0
Ivan Reshetnikov <ordinarydev@protonmail.com>
2024-03-05 23:46:44 +0500

    feat: update my website to v0.7.0

--- nixos/programs/comfycamp.nix
+++ nixos/programs/comfycamp.nix
@@ -2,7 +2,7 @@
 {
   virtualisation.oci-containers.containers.comfycamp = {
     autoStart = true;
-    image = "ghcr.io/ordinary-dev/comfycamp:v0.6.0";
+    image = "ghcr.io/ordinary-dev/comfycamp:v0.7.0";
     ports = ["55007:80"];
   };
 
