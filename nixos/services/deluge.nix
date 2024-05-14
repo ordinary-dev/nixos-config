6e83bff94a2dc97998e6a7450a4c5f99c8519bff
Ivan Reshetnikov <ordinarydev@protonmail.com>
2024-05-14 23:54:16 +0500

    feat: enable deluge web ui

--- nixos/services/deluge.nix
+++ nixos/services/deluge.nix
@@ -2,6 +2,21 @@
 {
   services.deluge = {
     enable = true;
-    web.enable = false;
+    web = {
+      enable = true;
+      port = 8112;
+    };
+  };
+  
+  services.nginx.virtualHosts."deluge.comfycamp.space" = {
+    useACMEHost = "comfycamp.space";
+    forceSSL = true;
+    listenAddresses = [
+      "10.101.0.1"
+    ];
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:${toString config.services.deluge.web.port}";
+      proxyWebsockets = true;
+    };
   };
 }
