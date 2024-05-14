e666c326ea36059c0eba3bf8ff7d54940eb47c64
Ivan Reshetnikov <ordinarydev@protonmail.com>
2024-05-14 23:07:07 +0500

    chore: hide grafana behind a vpn

--- nixos/monitoring/grafana.nix
+++ nixos/monitoring/grafana.nix
@@ -19,6 +19,9 @@
   services.nginx.virtualHosts.${config.services.grafana.settings.server.domain} = {
     useACMEHost = "comfycamp.space";
     forceSSL = true;
+    listenAddresses = [
+      "10.101.0.1"
+    ];
     locations."/" = {
       proxyPass = "http://127.0.0.1:${toString config.services.grafana.settings.server.http_port}";
       proxyWebsockets = true;
