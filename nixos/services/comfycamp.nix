4a7ece6fa4858b2c1bd5e6793c32be5c2ef6e864
Ivan Reshetnikov <ordinarydev@protonmail.com>
2024-09-21 13:54:25 +0500

    fix: update my website to v1.1.5

--- nixos/services/comfycamp.nix
+++ nixos/services/comfycamp.nix
@@ -7,7 +7,7 @@
   };
   virtualisation.oci-containers.containers.comfycamp-beta = {
     autoStart = true;
-    image = "git.comfycamp.space/lumin/comfycamp:v1.1.4";
+    image = "git.comfycamp.space/lumin/comfycamp:v1.1.5";
     ports = ["55407:4000"];
     user = "977:971";
     environmentFiles = [
